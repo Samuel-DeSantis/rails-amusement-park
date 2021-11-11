48a68f3c11f73beae7e7ab0ed9a406c37e0213d6
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-11-10 22:16:44 -0700

    commit tree 6847e6d10fce0c2a419865ea82cb97377c4653e7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+    skip_before_action :verified_user, only: [:new, :create]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        return redirect_to controller: 'users', action: 'new' unless @user.save
+        session[:user_id] = @user.id
+        redirect_to controller: 'welcome', action: 'home'
+    end
+
+    def show
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :password,
+            :height,
+            :happiness,
+            :nausea,
+            :admin,
+            :tickets
+        )
+    end
+
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,12 @@
 class Ride < ActiveRecord::Base
+
+    belongs_to :attraction
+    belongs_to :user
+
+    def take_ride
+        self.user.tickets -= 1
+        self.user.happiness += self.attraction.happiness_rating
+        self.user.nausea += self.attraction.nausea_rating
+    end
+
 end
